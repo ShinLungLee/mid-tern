0642cd21ce70486f2fc771362856125c1b855083
dennis1113chen <dennis1113.chen@gmail.com>
2023-04-10 15:49:53 +0800

    debug3.0

--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import style from './productitem.module.css';
+import style from './productItem.module.css';
 
 export default function ProductItem({ product }) {
 
