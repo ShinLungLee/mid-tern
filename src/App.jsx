77091542b9473768df83e46785d3cef3f7a3e769
KaLua30000 <beige30000@gmail.com>
2023-05-30 09:03:41 +0800

    Ll

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import Category from './pages/Category';
 import Home from './pages/Home'
 import Product from './pages/Product';
-import Login from './pages/login';
+import Login from './pages/Login';
 import Register from './pages/Register';
 import 'antd/dist/reset.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
