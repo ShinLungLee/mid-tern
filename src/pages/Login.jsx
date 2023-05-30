9a572b1b86653aab9011ee9e777ef14282263174
KaLua30000 <beige30000@gmail.com>
2023-05-30 09:06:06 +0800

    Ii

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import Signin from "../components/SignIn"
+import Signin from "../components/Signin"
 
 
 function Login() {
